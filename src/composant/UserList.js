4bfd0ee2bbd28925268af6671fa9a5c7a4c130c0
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-06-19 20:50:04 +0200

    Update UserList.js

--- src/composant/UserList.js
+++ src/composant/UserList.js
@@ -1,17 +1,29 @@
-import React,{useEffect,useState} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function UserList() {
-    const [data,setData] = useState([])
-    useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/users').then((Response)=>{setData(Response.data)
-      console.log(Response)
-      })
-    },[] )
+  // State variable to hold the data retrieved from the API
+  const [data, setData] = useState([]);
+
+  // useEffect hook used to perform side effects (API call) on component mount
+  useEffect(() => {
+    // Axios GET request to retrieve data from the specified URL
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        // Update the state with the retrieved data
+        setData(response.data);
+        console.log(response);
+      });
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <div>
-      {data.map((e)=>(<div> name is:{e.name}</div>))}
+      {/* Map over the data array and render the name for each user */}
+      {data.map((e) => (
+        <div> name is: {e.name} </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default UserList
+export default UserList;
